50c186ca63daca200e3d9c45c114003c8957f6e8
Terrence Bell <terrencekbell@gmail.com>
2020-03-22 14:58:08 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,7 +5,7 @@ class Triangle
      @line_3 = line_3
    end 
    def kind()
-    if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
+    if (@line_1 <= 0) || (@line_2 <= 0) || (@line_3 <= 0)
       raise TriangleError
     elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
       raise TriangleError
